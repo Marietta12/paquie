00754cb8e8d333fea241db0cf1862cfa91971d8c
Marietta12 <mariettaodile4@gmail.com>
2019-08-22 09:48:22 +0300

    modif by Marietta

--- resources/views/front/faqs/index.blade.php
+++ resources/views/front/faqs/index.blade.php
@@ -1,60 +1,28 @@
-    @extends('front.layout.master')
+@extends('front.layout.master')
 
-    @section('main')
-
+@section('main')
     	
 	<!-- Content page -->
 	<section class="bgwhite p-t-55 p-b-65">
 		<div class="container">
 			<div class="accordion" id="accordionExample">
-			@foreach($faqss as $faqs)
-		  <div class="card">
-		    <div class="card-header" id="headingOne">
-		      <h5 class="mb-0">
-		        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-		          {{ ($faqs) ? $faqs->questions : '' }}
-		        </button>
-		      </h5>
-		    </div>
-		    @endforeach
-		    <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
-		      <div class="card-body">
-		        {{ ($faqs) ? $faqs->answers : '' }}
-		      </div>
-		    </div>
-		  </div>
-		   
-		  <!-- <div class="card">
-		    <div class="card-header" id="headingTwo">
-		      <h5 class="mb-0">
-		        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-		          Collapsible Group Item #2
-		        </button>
-		      </h5>
-		    </div>
-		    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-		      <div class="card-body">
-		        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-		      </div>
-		    </div>
-		  </div> -->
-		 <!--  <div class="card">
-		    <div class="card-header" id="headingThree">
-		      <h5 class="mb-0">
-		        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-		          Collapsible Group Item #3
-		        </button>
-		      </h5>
-		    </div>
-		    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-		      <div class="card-body">
-		        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-		      </div>
-		    </div>
-		  </div> -->
-		</div>
-    </div>
-		</div>        
+			@foreach($faqss as $keys=>$faqs)
+			  <div class="card flex-c-m bg1 m-text3 trans-0-4">
+			    <div class="card-header" id="headingOne{{ $keys }}">
+			      <h5 class="mb-0">
+			        <button class="btn btn-link s-text7" type="button" data-toggle="collapse" data-target="#collapseOne{{ $keys }}" aria-expanded="true" aria-controls="collapseOne">
+			          {{ ($faqs) ? $faqs->questions : '' }}
+			        </button>
+			      </h5>
+			    </div>		    
+			    <div id="collapseOne{{ $keys }}" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+			      <div class="card-body">
+			        {{ ($faqs) ? $faqs->answers : '' }}
+			      </div>
+			    </div>
+			  </div>
+		 	 @endforeach  
+			</div>
+    	</div>        
 	</section>
-
 @endsection
